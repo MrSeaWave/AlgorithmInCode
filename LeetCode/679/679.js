637176a48dfbc7b00fe5963bfc0e5da4fd30c9aa
MrSeaWave <774866545@qq.com>
2018-09-13 23:02:06 +0800

    show 24

--- LeetCode/679/679.js
+++ LeetCode/679/679.js
@@ -49,6 +49,63 @@ var judgePoint24 = function(nums) {
   judgePoint(cards);
   return flag;
 };
-console.log(judgePoint24([4, 1, 8, 7]));
+
+/*
+* 展示算出24点的过程
+* */
+var judgePoint24Show = function(nums) {
+  let flag = false;
+  const cards = nums.map(i => i);
+  const process = nums.map(i => i);
+  let ans =[];
+  const judgePoint = (cards, process) => {
+    if (cards.length === 1) {
+      flag = Math.abs(cards[0] - 24.0) < 0.001;
+      ans = flag?[...ans,process[0]]:ans;
+    } else {
+      for (let i = 0; i < cards.length; i++) {
+        for (let j = 0; j < cards.length; j++) {
+          if (i === j) continue;
+          const operand0 = cards[i];
+          const operand1 = cards[j];
+          const tem = [...cards];
+          delete tem[i];
+          delete tem[j];
+          const subCards = [];
+          tem.forEach(i => subCards.push(i));
+          subCards.unshift(operand0 + operand1);
+
+          const process0 = process[i];
+          const process1 = process[j];
+          const temProcess = [...process];
+          delete temProcess[i];
+          delete temProcess[j];
+          const subProcess = [];
+          temProcess.forEach(i => subProcess.push(i));
+          subProcess.unshift(`(${process0} + ${process1})`);
+
+          judgePoint(subCards, subProcess);
+
+          subCards[0] = operand0 - operand1;
+          subProcess[0] = `(${process0} - ${process1})`;
+          judgePoint(subCards, subProcess);
+
+          subCards[0] = operand0 * operand1;
+          subProcess[0] = `(${process0} * ${process1})`;
+          judgePoint(subCards, subProcess);
+
+          if (operand1 !== 0) {
+            subCards[0] = operand0 / operand1;
+            subProcess[0] = `(${process0} / ${process1})`;
+            judgePoint(subCards, subProcess);
+          }
+        }
+      }
+    }
+  };
+  judgePoint(cards, process);
+  return {flag,ans};
+};
+console.log(judgePoint24Show([4, 1, 8, 7]));
 // 5 * (5 - 1 / 5)
 // 1 5 5 5
