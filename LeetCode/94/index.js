f476fb6648ee94bf1538595dee0c7e0c1d1c96af
MrSeaWave <774866545@qq.com>
2018-09-28 15:38:15 +0800

    94

--- LeetCode/94/index.js
+++ LeetCode/94/index.js
@@ -15,6 +15,14 @@
 const { makeTreeNode, TreeNode } = require("../utils/js/utils");
 
 var inorderTraversal = function(root) {
-
+  const ans = [];
+  const getMiddleTraverse = val => {
+    if (val === null) return;
+    getMiddleTraverse(val.left);
+    ans.push(val.val);
+    getMiddleTraverse(val.right);
+  };
+  getMiddleTraverse(root);
+  return ans;
 };
-console.log(inorderTraversal(makeTreeNode()))
+console.log(inorderTraversal(makeTreeNode(["A", "B", "C", "D", "E", "F"])));
