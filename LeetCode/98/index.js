8291c10dc9d1998eae9c6388738911ae7359ac37
MrSeaWave <774866545@qq.com>
2018-09-29 15:42:05 +0800

    98

--- LeetCode/98/index.js
+++ LeetCode/98/index.js
@@ -7,22 +7,42 @@
  */
 /**
  * @param {TreeNode} root
- * @return {number[]}
+ * @return {boolean}
  */
-/*
-* 中序遍历首先遍历左子树，然后访问根结点，最后遍历右子树
-* */
 const { makeTreeNode, TreeNode } = require("../utils/js/utils");
 
-var inorderTraversal = function(root) {
-  const ans = [];
-  const getMiddleTraverse = val => {
+/*
+* 中序遍历
+* */
+var isValidBST = function(root) {
+  const getMiddleTraverse = (val, ans = []) => {
     if (val === null) return;
-    getMiddleTraverse(val.left);
+    getMiddleTraverse(val.left, ans);
     ans.push(val.val);
-    getMiddleTraverse(val.right);
+    getMiddleTraverse(val.right, ans);
+    return ans;
   };
-  getMiddleTraverse(root);
-  return ans;
+  const list = getMiddleTraverse(root)||[];
+  for (let i = 0; i <= list.length - 2; i++) {
+    if (list[i] >= list[i + 1]) return false;
+  }
+  return true;
 };
-console.log(inorderTraversal(makeTreeNode(["A", "B", "C", "D", "E", "F"])));
+
+var isValidBST2 = function(root) {
+  // if (root === null) return true; // root=[]
+  const isValid = (val, left, right) => {
+    if (val === null) return true;
+    if (left !== null && val.val <= left) return false;
+    if (right !== null && val.val >= right) return false;
+    return (
+      isValid(val.left, left, val.val) && isValid(val.right, val.val, right)
+    );
+  };
+  return isValid(root, null, null);
+};
+// [10,5,15,null,null,6,20]
+// [0,-1]
+// [5,14,null,1]
+// [3,1,5,0,2,4,6]
+console.log(isValidBST(makeTreeNode([])));
