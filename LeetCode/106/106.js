cddc6cab6ea9b0a63313a86379c396a6cc4bbc23
MrSeaWave <774866545@qq.com>
2018-11-29 17:50:35 +0800

    从中序与后序遍历序列构造二叉树

---
+++ LeetCode/106/106.js
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {number[]} inorder
+ * @param {number[]} postorder
+ * @return {TreeNode}
+ */
+/*
+*
+* https://www.cnblogs.com/bmrs/archive/2010/08/19/SloveTree.html
+1 确定根,确定左子树，确定右子树。
+
+2 在左子树中递归。
+
+3 在右子树中递归。
+
+4 打印当前根。
+* */
+const {  TreeNode } = require('../utils/js/utils');
+
+var buildTree = function(inorder, postorder) {
+  if(inorder.length===0) return null
+  const recursiveFun=(inData,inStart,inEnd,postData,postStart,postEnd)=>{
+    if(inStart>inEnd||postEnd>postEnd) return null
+    const treeNode=new TreeNode(postData[postEnd])
+    for(let i=inStart;i<=inEnd;i++){
+      if(inData[i]===postData[postEnd]){
+        treeNode.left=recursiveFun(inData,inStart,i-1,postData,postStart,postStart+i-1-inStart)   //左子树
+        treeNode.right=recursiveFun(inData,i+1,inEnd,postData,postStart+i-inStart,postEnd-1)  //右子树
+      }
+    }
+    return treeNode
+  }
+  return recursiveFun(inorder,0,inorder.length-1,postorder,0,postorder.length-1)
+};
+console.log(buildTree([9, 3, 15, 20, 7], [9, 15, 7, 20, 3]));
