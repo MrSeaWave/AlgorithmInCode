e2dd03c5a5dc3e1ffc3e997b09c0eeea8bc4e56a
MrSeaWave <774866545@qq.com>
2020-03-19 10:57:38 +0800

    feat : 206

---
+++ LeetCode/206/index.js
@@ -0,0 +1,46 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+const { makeListNode, ListNode } = require('../utils/js/utils.js');
+
+var reverseList = function(head) {
+  if (!head) return head;
+  let result = new ListNode(head.val);
+  let nextNode = head.next;
+  while (nextNode && (nextNode.val === 0 || nextNode.val)) {
+    const newNode = new ListNode(nextNode.val);
+    newNode.next = result;
+    result = newNode;
+    nextNode = nextNode.next;
+  }
+  return result;
+};
+
+let reverseList1 = head => {
+  if (!head) return null;
+  let pre = null,
+    cur = head;
+  while (cur) {
+    console.log('cur',cur,pre)
+    // 保存下一个节点的值
+    let next = cur.next;
+    cur.next = pre;
+    pre = cur;
+    cur = next;
+  }
+  return pre;
+};
+
+const input = [[], [1, 2, 3, 4, 5], [1, 0, 2, 6, 8]];
+
+input.forEach(item => {
+  console.log('data', reverseList1(makeListNode(item)));
+});
