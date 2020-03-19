aa543087f5f7b8f763937b80d9c6e2f8866e256c
MrSeaWave <774866545@qq.com>
2020-03-19 20:48:09 +0800

    feat: 92

--- LeetCode/92/index.js
+++ LeetCode/92/index.js
@@ -14,21 +14,47 @@ const { makeListNode, ListNode } = require('../utils/js/utils.js');
 
 var reverseBetween = function(head, m, n) {
   if (!head) return null;
-  const count=n-m
-  let tem=new ListNode()
-  let result=tem
-  tem.next=head
+  let tem = new ListNode();
+  let result = tem;
+  tem.next = head;
   for (let i = 0; i < m - 1; i++) {
     tem = tem.next;
   }
-  let mFront=tem
-  let mHead=tem.next;
-
-  return result.next
+  // m位置之前的数据
+  let mFront = tem;
+  // m 位置的数据
+  let mHead = tem.next;
+  for (let i = m - 1; i < n; i++) {
+    tem = tem.next;
+  }
+  // n位置的数据
+  let nHead = tem;
+  // n 位置之后的数据
+  let nEnd = tem.next;
+  // 截断n位置数据
+  nHead.next = null;
+  // 普通反转函数，可参考206
+  let reverse = (cur, pre) => {
+    if (!cur) return pre;
+    const next = cur.next;
+    cur.next = pre;
+    pre = cur;
+    cur = next;
+    return reverse(cur, pre);
+  };
+  // 开始进行拼接
+  mFront.next = reverse(mHead, null);
+  // 经过上次反转后，m位置已经移到n位置了
+  mHead.next = nEnd;
+  return result.next;
 };
 
-const input = [{ head: [1, 2, 3, 4, 5], m: 2, n: 4 }];
+const input = [
+  { head: [1, 2, 3, 4, 5], m: 2, n: 4 },
+  { head: [1, 2, 3, 4, 5, 6, 7, 8], m: 1, n: 3 }
+];
 
 input.forEach(item => {
-  console.log('ans:', reverseBetween(makeListNode(item.head), item.m, item.n));
+  const ans = reverseBetween(makeListNode(item.head), item.m, item.n);
+  console.log('ans:', ans);
 });
