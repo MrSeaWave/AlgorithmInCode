7179e76129f6fb091e7825adde35d7255ad55824
MrSeaWave <774866545@qq.com>
2020-04-10 10:22:34 +0800

    feat : 21 增加一种解决思路

--- LeetCode/21/21.js
+++ LeetCode/21/21.js
@@ -5,8 +5,7 @@
  *     this.next = null;
  * }
  */
-const {makeListNode,ListNode} =require("../utils/js/utils")
-
+const { makeListNode, ListNode, parseListNodeToArray } = require('../utils/js/utils');
 
 /**
  * @param {ListNode} l1
@@ -15,29 +14,44 @@ const {makeListNode,ListNode} =require("../utils/js/utils")
  */
 
 /*
-* 思路：理解转换成链表的含义
-* */
-var mergeTwoLists = function(l1, l2) {
-    let head = new ListNode(0),
-        currentList = head;
-    while (l1 && l2) {
-        if (l1.val <= l2.val) {
-            currentList.next = l1;
-            l1 = l1.next;
-        } else {
-            currentList.next = l2;
-            l2 = l2.next;
-        }
-        // console.log('端点',currentList,currentList.next)
-        currentList = currentList.next;
+ * 思路：理解转换成链表的含义
+ * */
+var mergeTwoLists1 = function(l1, l2) {
+  let head = new ListNode(0),
+    currentList = head;
+  while (l1 && l2) {
+    if (l1.val <= l2.val) {
+      currentList.next = l1;
+      l1 = l1.next;
+    } else {
+      currentList.next = l2;
+      l2 = l2.next;
     }
-    if (l1) {
-        currentList.next = l1;
-    }
-    if (l2) {
-        currentList.next = l2;
-    }
-    return head.next;
+    // console.log('端点',currentList,currentList.next)
+    currentList = currentList.next;
+  }
+  if (l1) {
+    currentList.next = l1;
+  }
+  if (l2) {
+    currentList.next = l2;
+  }
+  return head.next;
 };
 
-console.log(mergeTwoLists(makeListNode([1,2,4]),makeListNode([1,3,4])))
+// 递归方法实现链表合并
+var mergeTwoLists = function(l1, l2) {
+  const merge = (l1, l2) => {
+    if (l1 === null) return l2;
+    if (l2 === null) return l1;
+    if (l1.val > l2.val) {
+      l2.next = merge(l1, l2.next);
+      return l2;
+    } else {
+      l1.next = merge(l1.next, l2);
+      return l1;
+    }
+  };
+  return merge(l1, l2);
+};
+console.log(parseListNodeToArray(mergeTwoLists(makeListNode([1, 2, 4]), makeListNode([1, 3, 4]))));
